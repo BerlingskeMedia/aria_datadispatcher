8b59358b8adae2a8a87878436fe38554f9c2c669
Daniel Jozie Kokott <dako@berlingskemedia.dk>
2019-06-11 16:39:18 +0200

    DISABLE_PAYLOAD_VALIDATION

--- server/scheme.js
+++ server/scheme.js
@@ -1,6 +1,7 @@
 /*jshint node: true */
 'use strict';
 
+const DISABLE_PAYLOAD_VALIDATION = (process.env.DISABLE_PAYLOAD_VALIDATION === 'true');
 
 if (process.env.NODE_ENV === 'test') {
   module.exports = require('../test/bpc_stub.js');
@@ -30,14 +31,23 @@ const scheme = function (server, options) {
         return Boom.badImplementation('Not connected to BPC');
       }
 
+      let payload = {
+        authorization: request.headers.authorization,
+        method: request.method,
+        url: request.url.href
+      };
+      
+      if(DISABLE_PAYLOAD_VALIDATION) {
+        // Do nothing
+      } else {
+        payload.payload = request.payload;
+        payload.contentType = request.contentType;
+      }
+
       const artifacts = await bpc.request({
         path: '/validate/credentials',
         method: 'POST',
-        payload: {
-          method: request.method,
-          url: request.url.href,
-          authorization: request.headers.authorization
-        }
+        payload: payload
       });
 
       return h.authenticated({ credentials: artifacts });
